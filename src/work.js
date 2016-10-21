4042eaa30b2a29975c710d1af94f40def14daa37
Artur Shelashskiy <a.shelashskiy@office.ngs.ru>
2016-10-21 21:05:17 +0700

    promise to generator changes

--- src/work.js
+++ src/work.js
@@ -1,4 +1,5 @@
 const co = require('co');
+const uuid = 'uuid';
 
 const fetchGeo = require('./fetch-geo');
 const fetchCoordinates = require('./fetch-coordinates');
@@ -11,6 +12,18 @@ const { WRITE_OUTPUT, CLEAN_QUEUE } = require('./constants');
 
 const imageDir = process.argv[4] || 'img';
 
+const workerPromise = (data) = new Promise((resolve, reject) => {
+    const type = `promise-${uuid.v1()}`;
+
+    process.on('message', message => {
+        if (message.type === type) {
+            resolve(message.result);
+        }
+    })
+
+    process.send({ type, payload: data });
+});
+
 module.exports = co.wrap(function* (id) {
     try {
         const geo = yield fetchGeo(id);
@@ -18,7 +31,7 @@ module.exports = co.wrap(function* (id) {
 
         const image = yield fetchImage(coordinates.centroid);
 
-        process.send({ type: WRITE_OUTPUT, result: {
+        yield workerPromise({ type: WRITE_OUTPUT, result: {
             id: geo.id,
             name: geo.name,
             boundingBox: coordinates.boundingBox
